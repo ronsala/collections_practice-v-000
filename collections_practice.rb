901bcdc1b6a91f1fd505e9feca2fa21193936237
Ron Sala <ron.sala@gmail.com>
2017-12-27 23:59:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(ary)
+  ary.sort
+end
 
+def sort_array_desc(ary)
+  ary.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(ary)
+  ary.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(ary)
+  ary
+end
